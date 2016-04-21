c4b18d8c108829a7825202aea52277977ff1f017
nindityawulandari <nindi.chan@gmail.com>
2016-04-21 21:01:20 +0700

    Added files via upload

---
+++ about.php
@@ -0,0 +1,545 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>about us</title>
+	<link rel="stylesheet" type="text/css" href="bootstrap-3.3.6-dist/css/bootstrap.min.css">
+	<link type="text/css" href="asset/style.css" rel="stylesheet">
+</head>
+<body>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">Event Publication</a>
+			</div>
+
+			<div id="navbar" class="navbar-collapse collapse">
+				<div id="navbar" class="collapse navbar-collapse">
+					<ul class="nav navbar-nav">
+						<li><a href="index.html">Home</a></li>
+						<li><a href="about.php">Menu List</a></li>
+						<li><a href="#contact">Contact Us</a></li>
+					</ul>
+					<form class="navbar-form navbar-right">
+						<div class="form-group">	
+							<input type="text" placeholder="search" class="form-control">
+							<div class="form-group">
+								<button type="submit" class="btn btn-default">cari</button>
+							</div>
+							<button type="submit" class="btn btn-success">log in</button>
+						</form>
+					</div><!-- /.navbar-collapse -->
+				</div><!--/.navbar-collapse -->
+			</nav>
+	
+			 <div id="cont" class="container">
+              				<div class="row">
+					<div class="col-xs-12 col-md-8  col-md-offset-2">
+					<div class="panel panel-default">
+  						<br><br><div class="panel-heading">
+					    <h1 class="panel-title"><h1>Rais</h1>
+					</div>
+					<div class="panel-body">
+					2016-04-02 21:10:33 oleh abdul<br />
+					<br/>
+					<div class="col-xs-12 col-md-6">
+	               	<img class="img-responsive" src="hiburan1.jpg"/>
+	 	  				
+	 	  			</div>	
+		  			<div class="col-xs-12 col-md-6">
+
+					<table class="table table-striped">
+					<tr>
+						<td>
+							<strong>Description</strong>
+						</td>
+						<td style="text-align: justify">
+							event ini diadakan oleh unnes yang akan di selenggarakan untuk mmringati					</td>
+					</tr>
+					<tr>
+						<td>
+							<strong>Time</strong>
+						</td>
+						<td>
+							0000-00-00						</td>
+					</tr>
+					<tr>
+						<td>
+							<strong>Place</strong>
+						</td>
+						<td>
+							fefe						</td>
+					</tr>
+					<tr>
+						<td>
+							<strong>Registration</strong>
+						</td>
+						<td>
+							efe						</td>
+					</tr>
+					<tr>
+						<td>
+							<strong>Contact Person</strong>
+						</td>
+						<td>
+							e						</td>
+					</tr>
+					</table>
+	  				</div>
+    
+				  </div>
+				</div>
+				</div>
+
+ 	  			
+	  
+	</div>
+                				<div class="row">
+					<div class="col-xs-12 col-md-8  col-md-offset-2">
+					<div class="panel panel-default">
+  						<div class="panel-heading">
+					    <h1 class="panel-title"><h1>asasas</h1>
+					</div>
+					<div class="panel-body">
+					2016-01-08 10:10:31 oleh admin<br />
+					<br/>
+					<div class="col-xs-12 col-md-6">
+	               	<img class="img-responsive" src="admin/dashboard/uploads/4a50a87cf737c81a4d612e37d8ca3b36.jpeg"/>
+	 	  				
+	 	  			</div>	
+		  			<div class="col-xs-12 col-md-6">
+
+					<table class="table table-striped">
+					<tr>
+						<td>
+							<strong>Description</strong>
+						</td>
+						<td style="text-align: justify">
+							asas						</td>
+					</tr>
+					<tr>
+						<td>
+							<strong>Time</strong>
+						</td>
+						<td>
+							0000-00-00						</td>
+					</tr>
+					<tr>
+						<td>
+							<strong>Place</strong>
+						</td>
+						<td>
+							asas						</td>
+					</tr>
+					<tr>
+						<td>
+							<strong>Registration</strong>
+						</td>
+						<td>
+							asas						</td>
+					</tr>
+					<tr>
+						<td>
+							<strong>Contact Person</strong>
+						</td>
+						<td>
+							asas						</td>
+					</tr>
+					</table>
+	  				</div>
+    
+				  </div>
+				</div>
+				</div>
+
+ 	  			
+	  
+	</div>
+                				<div class="row">
+					<div class="col-xs-12 col-md-8  col-md-offset-2">
+					<div class="panel panel-default">
+  						<div class="panel-heading">
+					    <h1 class="panel-title"><h1>Software Fair</h1>
+					</div>
+					<div class="panel-body">
+					2016-01-06 23:04:33 oleh Budi<br />
+					<br/>
+					<div class="col-xs-12 col-md-6">
+	               	<img class="img-responsive" src="admin/dashboard/uploads/pameran-software-fair-hmti-udinus-2015.jpg"/>
+	 	  				
+	 	  			</div>	
+		  			<div class="col-xs-12 col-md-6">
+
+					<table class="table table-striped">
+					<tr>
+						<td>
+							<strong>Description</strong>
+						</td>
+						<td style="text-align: justify">
+							Software Fair						</td>
+					</tr>
+					<tr>
+						<td>
+							<strong>Time</strong>
+						</td>
+						<td>
+							0000-00-00						</td>
+					</tr>
+					<tr>
+						<td>
+							<strong>Place</strong>
+						</td>
+						<td>
+							galery udinus						</td>
+					</tr>
+					<tr>
+						<td>
+							<strong>Registration</strong>
+						</td>
+						<td>
+							0						</td>
+					</tr>
+					<tr>
+						<td>
+							<strong>Contact Person</strong>
+						</td>
+						<td>
+							budi						</td>
+					</tr>
+					</table>
+	  				</div>
+    
+				  </div>
+				</div>
+				</div>
+
+ 	  			
+	  
+	</div>
+                				<div class="row">
+					<div class="col-xs-12 col-md-8  col-md-offset-2">
+					<div class="panel panel-default">
+  						<div class="panel-heading">
+					    <h1 class="panel-title"><h1>POD</h1>
+					</div>
+					<div class="panel-body">
+					2016-01-05 18:25:40 oleh doscom<br />
+					<br/>
+					<div class="col-xs-12 col-md-6">
+	               	<img class="img-responsive" src="admin/dashboard/uploads/4a50a87cf737c81a4d612e37d8ca3b36.jpeg"/>
+	 	  				
+	 	  			</div>	
+		  			<div class="col-xs-12 col-md-6">
+
+					<table class="table table-striped">
+					<tr>
+						<td>
+							<strong>Description</strong>
+						</td>
+						<td style="text-align: justify">
+							Pembekalan organisasi doscom						</td>
+					</tr>
+					<tr>
+						<td>
+							<strong>Time</strong>
+						</td>
+						<td>
+							0000-00-00						</td>
+					</tr>
+					<tr>
+						<td>
+							<strong>Place</strong>
+						</td>
+						<td>
+							bantir						</td>
+					</tr>
+					<tr>
+						<td>
+							<strong>Registration</strong>
+						</td>
+						<td>
+							50.000						</td>
+					</tr>
+					<tr>
+						<td>
+							<strong>Contact Person</strong>
+						</td>
+						<td>
+							diky						</td>
+					</tr>
+					</table>
+	  				</div>
+    
+				  </div>
+				</div>
+				</div>
+
+ 	  			
+	  
+	</div>
+                				<div class="row">
+					<div class="col-xs-12 col-md-8  col-md-offset-2">
+					<div class="panel panel-default">
+  						<div class="panel-heading">
+					    <h1 class="panel-title"><h1>release party</h1>
+					</div>
+					<div class="panel-body">
+					2016-01-02 11:31:05 oleh arnaz<br />
+					<br/>
+					<div class="col-xs-12 col-md-6">
+	               	<img class="img-responsive" src="admin/dashboard/uploads/Poster-Release-Party-TeaLinuxOS-7-Doscom-724x1024.png"/>
+	 	  				
+	 	  			</div>	
+		  			<div class="col-xs-12 col-md-6">
+
+					<table class="table table-striped">
+					<tr>
+						<td>
+							<strong>Description</strong>
+						</td>
+						<td style="text-align: justify">
+							tea linux os						</td>
+					</tr>
+					<tr>
+						<td>
+							<strong>Time</strong>
+						</td>
+						<td>
+							2012-12-12						</td>
+					</tr>
+					<tr>
+						<td>
+							<strong>Place</strong>
+						</td>
+						<td>
+							aula udinus						</td>
+					</tr>
+					<tr>
+						<td>
+							<strong>Registration</strong>
+						</td>
+						<td>
+							50.000						</td>
+					</tr>
+					<tr>
+						<td>
+							<strong>Contact Person</strong>
+						</td>
+						<td>
+							patricia						</td>
+					</tr>
+					</table>
+	  				</div>
+    
+				  </div>
+				</div>
+				</div>
+
+ 	  			
+	  
+	</div>
+                				<div class="row">
+					<div class="col-xs-12 col-md-8  col-md-offset-2">
+					<div class="panel panel-default">
+  						<div class="panel-heading">
+					    <h1 class="panel-title"><h1>Hours of Code</h1>
+					</div>
+					<div class="panel-body">
+					2015-12-29 05:54:48 oleh doscom<br />
+					<br/>
+					<div class="col-xs-12 col-md-6">
+	               	<img class="img-responsive" src="admin/dashboard/uploads/HOC.png"/>
+	 	  				
+	 	  			</div>	
+		  			<div class="col-xs-12 col-md-6">
+
+					<table class="table table-striped">
+					<tr>
+						<td>
+							<strong>Description</strong>
+						</td>
+						<td style="text-align: justify">
+							hours of code adalah hari dimana mengajak orang yang tidak terbiasa dengan koding untuk mengkoding						</td>
+					</tr>
+					<tr>
+						<td>
+							<strong>Time</strong>
+						</td>
+						<td>
+							0000-00-00						</td>
+					</tr>
+					<tr>
+						<td>
+							<strong>Place</strong>
+						</td>
+						<td>
+							Lab HW						</td>
+					</tr>
+					<tr>
+						<td>
+							<strong>Registration</strong>
+						</td>
+						<td>
+							free						</td>
+					</tr>
+					<tr>
+						<td>
+							<strong>Contact Person</strong>
+						</td>
+						<td>
+							Zakky						</td>
+					</tr>
+					</table>
+	  				</div>
+    
+				  </div>
+				</div>
+				</div>
+
+ 	  			
+	  
+	</div>
+                				<div class="row">
+					<div class="col-xs-12 col-md-8  col-md-offset-2">
+					<div class="panel panel-default">
+  						<div class="panel-heading">
+					    <h1 class="panel-title"><h1>Doscom University</h1>
+					</div>
+					<div class="panel-body">
+					2015-12-29 00:46:03 oleh admin<br />
+					<br/>
+					<div class="col-xs-12 col-md-6">
+	               	<img class="img-responsive" src="admin/dashboard/uploads/poster.png"/>
+	 	  				
+	 	  			</div>	
+		  			<div class="col-xs-12 col-md-6">
+
+					<table class="table table-striped">
+					<tr>
+						<td>
+							<strong>Description</strong>
+						</td>
+						<td style="text-align: justify">
+							pelatihan open source						</td>
+					</tr>
+					<tr>
+						<td>
+							<strong>Time</strong>
+						</td>
+						<td>
+							2016-06-15						</td>
+					</tr>
+					<tr>
+						<td>
+							<strong>Place</strong>
+						</td>
+						<td>
+							Lab HW						</td>
+					</tr>
+					<tr>
+						<td>
+							<strong>Registration</strong>
+						</td>
+						<td>
+							30.000						</td>
+					</tr>
+					<tr>
+						<td>
+							<strong>Contact Person</strong>
+						</td>
+						<td>
+							Diky 08512312123						</td>
+					</tr>
+					</table>
+	  				</div>
+    
+				  </div>
+				</div>
+				</div>
+
+ 	  			
+	  
+	</div>
+                				<div class="row">
+					<div class="col-xs-12 col-md-8  col-md-offset-2">
+					<div class="panel panel-default">
+  						<div class="panel-heading">
+					    <h1 class="panel-title"><h1>Bootcamp Doscom 15</h1>
+					</div>
+					<div class="panel-body">
+					2015-12-29 00:33:06 oleh atho<br />
+					<br/>
+					<div class="col-xs-12 col-md-6">
+	               	<img class="img-responsive" src="admin/dashboard/uploads/bootcamp.png"/>
+	 	  				
+	 	  			</div>	
+		  			<div class="col-xs-12 col-md-6">
+
+					<table class="table table-striped">
+					<tr>
+						<td>
+							<strong>Description</strong>
+						</td>
+						<td style="text-align: justify">
+							Makrab Organisasi Doscom						</td>
+					</tr>
+					<tr>
+						<td>
+							<strong>Time</strong>
+						</td>
+						<td>
+							2015-11-14						</td>
+					</tr>
+					<tr>
+						<td>
+							<strong>Place</strong>
+						</td>
+						<td>
+							Bandungan						</td>
+					</tr>
+					<tr>
+						<td>
+							<strong>Registration</strong>
+						</td>
+						<td>
+							50.000						</td>
+					</tr>
+					<tr>
+						<td>
+							<strong>Contact Person</strong>
+						</td>
+						<td>
+							Zakky						</td>
+					</tr>
+					</table>
+	  				</div>
+    
+				  </div>
+				</div>
+				</div>
+
+ 	  			
+	  
+	</div>
+                              <div colspan="7" align="center">
+                <div class="pagination-wrap">
+                  <ul class="pagination"><li><a href='/index.php?page_no=1' style='color:red;'>1</a></li></ul>                </div>
+              </div>
+    </div>
+
+
+      <footer>
+        <p>&copy; Company 2014</p>
+      </footer>
+    </div>
+    </div>
+  </body>
+
+</body>
+</html>
---
+++ contactus.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>about us</title>
+	<link rel="stylesheet" type="text/css" href="bootstrap-3.3.6-dist/css/bootstrap.min.css">
+</head>
+<body>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">Event Publication</a>
+			</div>
+
+			<div id="navbar" class="navbar-collapse collapse">
+				<div id="navbar" class="collapse navbar-collapse">
+					<ul class="nav navbar-nav">
+						<li><a href="index.html">Home</a></li>
+						<li><a href="about.php">Menu List</a></li>
+						<li><a href="#contact">Contact Us</a></li>
+					</ul>
+					<form class="navbar-form navbar-right">
+						<div class="form-group">	
+							<input type="text" placeholder="search" class="form-control">
+							<div class="form-group">
+								<button type="submit" class="btn btn-default">cari</button>
+							</div>
+							<button type="submit" class="btn btn-success">log in</button>
+						</form>
+					</div><!-- /.navbar-collapse -->
+				</div><!--/.navbar-collapse -->
+			</nav>
+</body>
+</html>
